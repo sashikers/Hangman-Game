8572bf9e0ee7b7136fe922a3dab0d2911ed71d40
Sasha <sashikers@gmail.com>
2017-11-16 19:44:34 -0800

    use strict

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,36 @@
-var breweries = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
-console.log(breweries);
+(function () {
+	"use strict";
+	var availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched, output, man, letters, lives, currentWord, numLettersMatched, messages;
+	console.log("hello");
+	function setup() {
+		// Start config options
+		availableLetters = "abcdefghijklmnopqrstuvwxyz";
+		lives = 6;
+		words = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
+		messages = {
+			win: "Awesome! Have a beer!",
+			lose: "Oh no! Try again!",
+			guessed: "You already tried that one! Bozo!",
+			validLetter: "Try a valid letter, please!",
+		};
+		// end config 
+
+		lettersGuessed = lettersMatched = '';
+		numLettersMatched = 0;
+
+		// selects a word
+		currentWord = words[Math.floor(Math.random()* words.length)];
+		console.log(currentWord);
+
+
+	}
+}
+)
+
+// var breweries = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
+// console.log(breweries);
+
+// document.onkeyup = function(event){
+// 	console.log(event);
+
+// }
