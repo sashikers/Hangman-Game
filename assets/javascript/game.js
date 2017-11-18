ea361fe725ebc727b861feb5c14a908811641421
Sasha <sashikers@gmail.com>
2017-11-17 16:47:34 -0800

    typed letters now appear on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
 var words = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
+var allGuesses = document.getElementById("allGuesses");
+
 
 console.log(words);
 
 function makeMan() {
-	var currentWord = words[Math.floor(Math.random()*words.length)]
+	var currentWord = words[Math.floor(Math.random()*words.length)];
+	currentWord = currentWord.toUpperCase();
 	console.log(currentWord);
 	var currentWordArray = currentWord.split("");
 	console.log(currentWordArray);
@@ -11,5 +14,14 @@ function makeMan() {
 
 makeMan();
 
+document.onkeyup = function(event) {
+	var userInput = String.fromCharCode(event.keyCode).toUpperCase();
+	console.log(userInput);
+	var guessedLetter = document.createElement("span");
+	guessedLetter.setAttribute("class", "guessedButtons");
+	var textGuessedLetter = document.createTextNode(userInput);
+	guessedLetter.appendChild(textGuessedLetter);
+	document.getElementById("allGuesses").appendChild(guessedLetter);
+}
 
 	
