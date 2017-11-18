5e4cf6278b088e498fdc42d741ea7f65f256b721
Sasha <sashikers@gmail.com>
2017-11-17 16:03:51 -0800

    made word array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,15 @@
-(function () {
-	"use strict";
-	var availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched, output, man, letters, lives, currentWord, numLettersMatched, messages;
-	console.log("hello");
-	function setup() {
-		// Start config options
-		availableLetters = "abcdefghijklmnopqrstuvwxyz";
-		lives = 6;
-		words = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
-		messages = {
-			win: "Awesome! Have a beer!",
-			lose: "Oh no! Try again!",
-			guessed: "You already tried that one! Bozo!",
-			validLetter: "Try a valid letter, please!",
-		};
-		// end config 
+var words = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
 
-		lettersGuessed = lettersMatched = '';
-		numLettersMatched = 0;
+console.log(words);
 
-		// selects a word
-		currentWord = words[Math.floor(Math.random()* words.length)];
-		console.log(currentWord);
-
-
-	}
+function makeMan() {
+	var currentWord = words[Math.floor(Math.random()*words.length)]
+	console.log(currentWord);
+	var currentWordArray = currentWord.split("");
+	console.log(currentWordArray);
 }
-)
 
-// var breweries = ["Anchor", "Almanac", "Barebottle", "Cellarmaker", "Harmonic", "Magnolia", "ThirstyBear"];
-// console.log(breweries);
+makeMan();
 
-// document.onkeyup = function(event){
-// 	console.log(event);
 
-// }
+	
